d73e50ee60e89859bf4ecfa155b69a9a60e335cd
oladanish545-svg <oladanish545@gmail.com>
2026-01-16 09:51:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+﻿setTimeout(()=>{
+document.getElementById("splash").classList.add("hidden");
+document.getElementById("app").classList.remove("hidden");
+},2000);
+
+document.getElementById("compareBtn").onclick=()=>{
+const map=document.getElementById("map");
+document.getElementById("loading").classList.remove("hidden");
+
+map.style.transition="transform 3s ease";
+map.style.transform="rotate(6deg) scale(1.05)";
+
+setTimeout(()=>{
+document.getElementById("loading").classList.add("hidden");
+document.getElementById("mapSection").classList.add("hidden");
+document.getElementById("results").classList.remove("hidden");
+},3500);
+};
